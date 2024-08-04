8b95caf6f72c7ac76fbac69274fc78c990aeaad0
Caio <njtrio7@gmail.com>
2024-08-03 22:02:24 -0200

    git push

--- CALCULADOR EM JAVA/src/Principal.java
+++ CALCULADOR EM JAVA/src/Principal.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 public class Principal {
     public static void main(String[] args) {
         EventQueue.invokeLater(new Runnable() {
+            @Override
             public void run (){
                 try{
                     calculadoraDark frame = new calculadoraDark();
